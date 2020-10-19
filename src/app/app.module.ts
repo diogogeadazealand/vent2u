2d89b2b9f8321c680efe1ffd60e4f8bb140adc73
Jesper Pedersen <jeppenp12@gmail.com>
2020-10-19 13:15:19 +0200

    Basic Layout for Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Main Dashboard --TEMPERATURE--
+import { DashboardMain } from './main/main.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardMain
   ],
   imports: [
     BrowserModule,
