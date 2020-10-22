630d4657d5af1b5645a24d99fde08ce9e335b55e
Jesper Pedersen <jeppenp12@gmail.com>
2020-10-22 12:02:42 +0200

    Presets feature

---
+++ src/app/temperature/Presets/presets.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+@Component({
+    selector: '<presets>',
+    templateUrl: './presets.component.html',
+    styleUrls: ['./presets.component.css']
+})
+export class Presets {
+} 
--- src/app/temperature/temperature.module.ts
+++ src/app/temperature/temperature.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 // Material Design
-import {MatMenuModule} from '@angular/material/menu';
+import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSliderModule} from '@angular/material/slider';
 
@@ -12,19 +12,21 @@ import {MatSliderModule} from '@angular/material/slider';
 // Custom Components
 import { Temperature } from './temperature.component';
 import { DashboardSlider } from './Slider/slider.component';
+import { Presets } from './Presets/presets.component';
 
 
 @NgModule({
   declarations: [
     Temperature,
-    DashboardSlider
+    DashboardSlider,
+    Presets
     ],
   exports: [
   ],
   imports: [
     CommonModule,
     FormsModule,
-    MatMenuModule,
+    MatSelectModule,
     MatIconModule,
     MatSliderModule
   ]
