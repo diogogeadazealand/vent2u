d816a51f1bbcdce51b7553f384b66188f4fbf332
Hazeha <krantja@gmail.com>
2020-10-23 16:39:58 +0200

    ,

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Login Guard
+import { AuthGuard } from './auth/auth.guard';
+
+// Main Routes - Depending on login status.
+import { NotFoundComponent} from './404/notfound.component'; // Unknown page
+import { LoginComponent } from './auth/login/login.component'; // Login for dashboard
+import { DashboardComponent } from './dashboard/dashboard.component'; // This is the UI behind Auth - This is our main view.
+
+// Child Routes Dashboard
+import { MainComponent } from './dashboard/main/main.component'; // Page1
+import { VentsComponent } from './dashboard/vents/vents.component'; // Page2
+import { ClaimComponent } from './dashboard/claim/claim.component';
+import { Temperature } from './temperature/temperature.component'; // Page 3
+
+
+const routes: Routes = [
+  { path: 'Dashboard', component: DashboardComponent, canLoad: [AuthGuard],
+    children: [
+      { path: 'Main', component: Temperature, },
+      { path: 'Vents', component: VentsComponent },
+      { path: 'Claim', component: ClaimComponent }
+    ] },
+  { path: '',   component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Main UI
+import { NotFoundComponent } from './404/notfound.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+// Modules TODO Should be moved to dashboard.
+import { DashboardTemperature } from './temperature/temperature.module';
+import {AuthModule} from './auth/auth.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DashboardTemperature,
+    DashboardModule,
+    AuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
