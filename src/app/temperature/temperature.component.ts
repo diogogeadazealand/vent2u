838b0b600c73aa4791dba3864a02f7350116925e
Hazeha <krantja@gmail.com>
2020-10-23 16:31:26 +0200

    .

--- src/app/temperature/temperature.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'app-temperature',
-    templateUrl: './temperature.component.html',
-    styleUrls: ['./temperature.component.css']
-})
-export class Temperature {
-} 
