80801fdac2c3413749a6703b3d85436bd0a102bb
Hazeha <krantja@gmail.com>
2020-10-23 13:38:30 +0200

    buityfy

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import {MatIconModule} from '@angular/material/icon';
-import {RouterModule} from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 import { MainComponent } from './main/main.component';
@@ -15,14 +15,15 @@ import { ClaimComponent } from './claim/claim.component';
 @NgModule({
   declarations: [
     DashboardComponent,
+    // Pages
     MainComponent,
     VentsComponent,
     ClaimComponent
   ],
   imports: [
     CommonModule,
-    MatIconModule,
     RouterModule,
+    MatIconModule,
     MatToolbarModule
   ]
 })
