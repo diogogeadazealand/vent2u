dd170371d1607d927d5ebead9382a4c4828869b0
Hazeha <krantja@gmail.com>
2020-10-23 13:36:06 +0200

    App.Module Clean Up
    Deleted unused import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,47 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Material Design
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-
-// Custom Views
-import { PageNotFoundComponent } from './404.component';
+// Main UI
+import { NotFoundComponent } from './404/notfound.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 
-// auth
-
-
-// Modules
+// Modules TODO Should be moved to dashboard.
 import { DashboardTemperature } from './main/temperature.module';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import { NotfoundComponent } from './404/notfound.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
-    NotfoundComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatMenuModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatFormFieldModule,
     DashboardTemperature,
-    ReactiveFormsModule,
-
-    MatButtonModule,
     DashboardModule
   ],
   providers: [],
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,6 +4,8 @@ import { DashboardComponent } from './dashboard.component';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 import { MainComponent } from './main/main.component';
 import { VentsComponent } from './vents/vents.component';
 import { ClaimComponent } from './claim/claim.component';
