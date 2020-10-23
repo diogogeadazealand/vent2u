07da46b4edd7a7e55b9f9016dcee8c2a53b12080
Hazeha <krantja@gmail.com>
2020-10-23 16:33:05 +0200

    Revert "."
    This reverts commit 838b0b600c73aa4791dba3864a02f7350116925e.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Pages
+import { PageNotFoundComponent } from './notfound.component';
+import { Temperature } from './temperature/temperature.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: Temperature },
+  { path: 'Temperature', component: Temperature },
+  { path: '**', component: PageNotFoundComponent } // Wildcard
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+
+
+@Component({
+    selector: 'login',
+    template: `
+    <p>
+    <mat-form-field appearance="legacy">
+      <mat-label>Legacy form field</mat-label>
+      <input matInput placeholder="Placeholder">
+      <mat-icon matSuffix>sentiment_very_satisfied</mat-icon>
+      <mat-hint>Hint</mat-hint>
+    </mat-form-field>
+  </p>
+    `,
+
+    styleUrls: ['./login.component.css'],
+    providers: []    
+})
+export class Login {
+} 
