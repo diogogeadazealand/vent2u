9048c79144b9e385586ac7ad3fe1bc3f6c760a3f
Jesper Pedersen <jeppenp12@gmail.com>
2020-10-29 12:23:37 +0100

    Rename Presets to PresetsComponent to make it easier to identify

--- src/app/shared/_components/Presets/presets.module.ts
+++ src/app/shared/_components/Presets/presets.module.ts
@@ -3,17 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { Presets } from './presets.component';
+import { PresetsComponent } from './presets.component';
 
 // Material Design
 import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    Presets
-  ],
+    PresetsComponent  ],
   exports: [
-    Presets
+    PresetsComponent
   ],
   imports: [
     CommonModule,
