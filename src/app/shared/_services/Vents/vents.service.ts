b6d61be95a95c67a0f1b178506b9dfe0a884184c
Jesper Pedersen <jeppenp12@gmail.com>
2020-11-13 12:18:35 +0100

    API Put: Saves now to database

--- src/app/shared/_services/Vents/vents.service.ts
+++ src/app/shared/_services/Vents/vents.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const baseUrl = 'http://localhost:8080/vent2u/vents';
+
 @Injectable()
 export class VentsService {
   constructor(private http: HttpClient) { }
-  getVentData(id) {
-    const url = "http://localhost:8080/vent2u/vents/" + id;
-    return this.http.get(url);
+
+  getAll() {
+    return this.http.get(baseUrl);
+  }
+
+  getOne(id) {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data) {
+    return this.http.post(baseUrl, data); 
+  }
+
+  update(data) {
+    return this.http.put(`${baseUrl}/`, data);
+  }
+
+  delete(id) {
+    return this.http.delete(`${baseUrl}/${id}`);    
   }
 }
